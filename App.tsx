be9b74e962cb24aa756fc22410716d6de2576b07
AliSabet81 <sabet.farshid1381@gmail.com>
2024-05-12 16:42:13 +0330

    publishing

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  let x = 1;
   return (
     <View style={styles.container}>
-      <Text>hello ali!</Text>
+      <Text>hello there ali!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
+  }, 
 });
