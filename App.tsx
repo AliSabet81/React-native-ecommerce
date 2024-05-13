913c34b0bd79373211f48277d57d6836d215f23c
AliSabet81 <sabet.farshid1381@gmail.com>
2024-05-14 01:12:21 +0330

    custom Text component

--- App.tsx
+++ App.tsx
@@ -2,9 +2,15 @@ import { StatusBar } from "expo-status-bar";
 import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import AppText from "./app/components/AppText";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View style={styles.container}>
+      <AppText>hello</AppText>
+      <Text>hello</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
