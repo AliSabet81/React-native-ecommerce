94c1a38412b060a175bba8a3cde55bd16948f1e9
AliSabet81 <sabet.farshid1381@gmail.com>
2024-05-15 13:55:31 +0330

    Messages Screen

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import { StyleSheet } from "react-native";
 import React from "react";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <MessagesScreen />;
 }
 
 const styles = StyleSheet.create({
